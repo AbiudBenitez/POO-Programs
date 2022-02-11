74158f2b9db089111e75cecca3078992ce2cdbd0
abiudbenitez <abiudmisael10@hotmail.com>
2022-02-11 10:30:29 -0600

    Se completo el ejercicio

--- src/semana2/Funciones3.java
+++ src/semana2/Funciones3.java
@@ -1,5 +1,27 @@
 package semana2;
 
-public class Funciones3 {
+import java.util.Scanner;
 
+public class Funciones3 extends Funciones2 {
+	protected double[] qtss(Scanner sc) {
+		double[] r = new double[2];
+		System.out.print("Ingrese los datos de la variable G: ");
+		r[0] = sc.nextDouble();
+		System.out.print("Ingrese los datos de la variable E: ");
+		r[1] = sc.nextDouble();
+		sc.close();
+		return r;
+	}
+	protected double op6(double z, double a, double b) {
+		double j = (Math.cbrt(z)/3d*a*b);
+		return j;
+	}
+	protected double op7(double w, double m, double a, double b) {
+		double k = (Math.pow(w + m, 2)/4d*a*Math.pow(b, 2));
+		return k;
+	}
+	protected double op8(double g, double a, double e, double x, double w, double y) {
+		double p = ((g*Math.pow(a, 3)-1d/3d*e)/Math.pow(x, 2)-(w*y));
+		return p;
+	}
 }
