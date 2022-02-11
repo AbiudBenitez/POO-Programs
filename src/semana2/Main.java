c017af598df9b3164cf131828f92feeca9e1f080
abiudbenitez <abiudmisael10@hotmail.com>
2022-02-11 10:30:53 -0600

    Se conectaron todas las partes

--- src/semana2/Main.java
+++ src/semana2/Main.java
@@ -1,20 +1,23 @@
 package semana2;
 
-public class Main extends Funciones2 {
+public class Main extends Funciones3 {
 	public static void main(String[] args) {; 
 		double x, y, z, w, m;
-		Funciones fun = new Funciones();
-		Funciones2 fun2 = new Funciones2();
-		double []q = fun.qts();
+		Funciones3 fun = new Funciones3();
+		double[] q = fun.qts(fun.scn());
+		double[] r = fun.qtss(fun.scn());
 		x = fun.op1(q[0], q[1]);
 		y = fun.op2(q[1], q[2]);
 		z = fun.op3(q[0], q[1], q[2]);
-		w = fun2.op4(y, q[2]);
-		m = fun2.op5(x, q[1], z, y);
+		w = fun.op4(y, q[2]);
+		m = fun.op5(x, q[1], z, y);
 		System.out.println("El resultado de X es: " + x);
 		System.out.println("El resultado de Y es: " + y);
 		System.out.println("El resultado de Z es: " + z);
 		System.out.println("El resultado de W es: " + w);
 		System.out.println("El resultado de M es: " + m);
+		System.out.println("El resultado de J es: " + fun.op6(z, q[0], q[1]));
+		System.out.println("El resultado de K es: " + fun.op7(w, m, q[0], q[1]));
+		System.out.println("El resultado de J es: " + fun.op8(r[0], q[0], r[1], x, w, y));
 	}
 }
