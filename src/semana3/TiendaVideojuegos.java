577b9005ee8e5bcafe4a49f5b9684b1c7e981d8a
abiudbenitez <abiudmisael10@hotmail.com>
2022-02-17 20:42:42 -0600

    Se hizo una parte del ejercicio

---
+++ src/semana3/TiendaVideojuegos.java
@@ -0,0 +1,9 @@
+package semana3;
+
+public class TiendaVideojuegos {
+
+	public static void main(String[] args) {
+		TiendaVideojuegos TV = new TiendaVideojuegos();
+	}
+
+}
---
+++ src/semana3/Videojuegos.java
@@ -0,0 +1,19 @@
+package semana3;
+
+public class Videojuegos {
+	int cve_vid;
+	String nom_vid;
+	String gen_vid;
+	float pre_vid;
+	int inv_vid;
+	String prov_vid;
+	
+	public Videojuegos(int cve_vid, String nom_vid, String gen_vid, float pre_vid, int inv_vid, String prov_vid) {
+		this.cve_vid = cve_vid;
+		this.nom_vid = nom_vid;
+		this.gen_vid = gen_vid;
+		this.pre_vid = pre_vid;
+		this.inv_vid = inv_vid;
+		this.prov_vid = prov_vid;
+	}
+}
